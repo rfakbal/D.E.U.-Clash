df3618ffdceefc69888804d5e94e16777ae9fd1e
RusenAkbal <rusenfirat@gmail.com>
2024-10-30 19:46:06 +0300

    Computer Added
    Code that satisfies computer movement has been added to code but needs be improved.

--- Program.cs
+++ Program.cs
@@ -937,5 +937,172 @@ public class Program
             if (printSeq) Console.WriteLine("Col3:Seq.11 20 Points");
             tablePoint += 20;
         }
+
+        // Code block of the Computer move
+        int maxPoint = 0, maxPosition = 0, maxColor = 0, maxValue = 0;
+        Random rd = new Random();
+        printSeq = false;
+    
+        for (int i = 0; i < 25; i++) {
+            // Assigning random values
+            int tempValue = rd.Next(3) + 1;
+            int tempColor = rd.Next(3) + 1;
+            int tempPosition = rd.Next(9);
+    
+            // Creating temp values
+            int originalValue = 0;
+            int originalColor = 0;
+    
+            switch (tempPosition) {
+                case 0:
+                    originalValue = pos1Value;
+                    originalColor = pos1Color;
+                    pos1Value = tempValue;
+                    pos1Color = tempColor;
+                    break;
+                case 1:
+                    originalValue = pos2Value;
+                    originalColor = pos2Color;
+                    pos2Value = tempValue;
+                    pos2Color = tempColor;
+                    break;
+                case 2:
+                    originalValue = pos3Value;
+                    originalColor = pos3Color;
+                    pos3Value = tempValue;
+                    pos3Color = tempColor;
+                    break;
+                case 3:
+                    originalValue = pos4Value;
+                    originalColor = pos4Color;
+                    pos4Value = tempValue;
+                    pos4Color = tempColor;
+                    break;
+                case 4:
+                    originalValue = pos5Value;
+                    originalColor = pos5Color;
+                    pos5Value = tempValue;
+                    pos5Color = tempColor;
+                    break;
+                case 5:
+                    originalValue = pos6Value;
+                    originalColor = pos6Color;
+                    pos6Value = tempValue;
+                    pos6Color = tempColor;
+                    break;
+                case 6:
+                    originalValue = pos7Value;
+                    originalColor = pos7Color;
+                    pos7Value = tempValue;
+                    pos7Color = tempColor;
+                    break;
+                case 7:
+                    originalValue = pos8Value;
+                    originalColor = pos8Color;
+                    pos8Value = tempValue;
+                    pos8Color = tempColor;
+                    break;
+                case 8:
+                    originalValue = pos9Value;
+                    originalColor = pos9Color;
+                    pos9Value = tempValue;
+                    pos9Color = tempColor;
+                    break;
+            }
+    
+            tablePoint = 0;
+            // there should we should calculate the table point WILL BE ADDED
+    
+            // depending on the values, updating the maxPoint
+            if (tablePoint > maxPoint) {
+                maxPoint = tablePoint;
+                maxPosition = tempPosition;
+                maxColor = tempColor;
+                maxValue = tempValue;
+            }
+    
+            // changing back the old values
+            switch (tempPosition) {
+                case 0:
+                    pos1Value = originalValue;
+                    pos1Color = originalColor;
+                    break;
+                case 1:
+                    pos2Value = originalValue;
+                    pos2Color = originalColor;
+                    break;
+                case 2:
+                    pos3Value = originalValue;
+                    pos3Color = originalColor;
+                    break;
+                case 3:
+                    pos4Value = originalValue;
+                    pos4Color = originalColor;
+                    break;
+                case 4:
+                    pos5Value = originalValue;
+                    pos5Color = originalColor;
+                    break;
+                case 5:
+                    pos6Value = originalValue;
+                    pos6Color = originalColor;
+                    break;
+                case 6:
+                    pos7Value = originalValue;
+                    pos7Color = originalColor;
+                    break;
+                case 7:
+                    pos8Value = originalValue;
+                    pos8Color = originalColor;
+                    break;
+                case 8:
+                    pos9Value = originalValue;
+                    pos9Color = originalColor;
+                    break;
+            }
+        }
+    
+        // updating the best scenario
+        if (maxPosition != -1) {
+            switch (maxPosition) {
+                case 0:
+                    pos1Value = maxValue;
+                    pos1Color = maxColor;
+                    break;
+                case 1:
+                    pos2Value = maxValue;
+                    pos2Color = maxColor;
+                    break;
+                case 2:
+                    pos3Value = maxValue;
+                    pos3Color = maxColor;
+                    break;
+                case 3:
+                    pos4Value = maxValue;
+                    pos4Color = maxColor;
+                    break;
+                case 4:
+                    pos5Value = maxValue;
+                    pos5Color = maxColor;
+                    break;
+                case 5:
+                    pos6Value = maxValue;
+                    pos6Color = maxColor;
+                    break;
+                case 6:
+                    pos7Value = maxValue;
+                    pos7Color = maxColor;
+                    break;
+                case 7:
+                    pos8Value = maxValue;
+                    pos8Color = maxColor;
+                    break;
+                case 8:
+                    pos9Value = maxValue;
+                    pos9Color = maxColor;
+                    break;
+            }
+        }
+        printSeq = true;
     }
 }
